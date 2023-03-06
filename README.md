c08852fc7d6cf01facfc781ec0af2a0f1f634663
Fender, Patrick <patricktfender+git@gmail.com>
2023-03-06 10:39:48 +0100

    configure vite proxy

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ Start db:
 - put postgres url in .env
 - docker-compose up
 
-Start node:
+Start backend:
 
 - npm i
 - npx prisma migrate dev --name init
 - npm run dev
+
+Start frontend:
+
+- put VITE_API_BASE_URL="localhost:7000" in .env
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import http from 'https';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // add backend as proxy
-  // use with frontend-URL.com/api/...
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    // add backend as proxy
+    // use with frontend-URL.com/api/...
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          agent: new http.Agent(),
+        },
+      },
+    },
+  };
 });
