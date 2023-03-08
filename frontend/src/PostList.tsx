575b196acc73c6ee9f9caa01d66f4106eb59e5f1
Fender, Patrick <patricktfender+git@gmail.com>
2023-03-08 16:50:54 +0100

    infinite scroll automatically

--- frontend/src/PostList.tsx
+++ frontend/src/PostList.tsx
@@ -1,5 +1,5 @@
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   getAllPosts,
   getPostsPaginated,
@@ -14,8 +14,36 @@ function PostList({}: Props) {
     queryKey: ['myposts'],
     getNextPageParam: (prevData) => prevData.nextPage,
     queryFn: ({ pageParam = 1 }) =>
-      getPostsPaginated(pageParam, 3, 'createdDate'), // pageParam is returned by getNextPageParam
+      getPostsPaginated(pageParam, 5, 'createdDate'), // pageParam is returned by getNextPageParam
   });
+
+  const intersectObserver = useRef<IntersectionObserver>();
+  const lastPostRef = useCallback(
+    // called when the last post is rendered
+    (post: HTMLLIElement | null) => {
+      if (postsQuery.isLoading) {
+        return;
+      }
+
+      // attach an IntersectionObserver to the new last post
+      // first disconnect from the previous last post
+      if (intersectObserver.current) {
+        intersectObserver.current.disconnect();
+      }
+      intersectObserver.current = new IntersectionObserver(
+        (posts) => {
+          // detect when the last post in the list is intersecting with the viewport
+          if (posts[0].isIntersecting && postsQuery.hasNextPage) {
+            postsQuery.fetchNextPage();
+          }
+        }
+      );
+      if (post) {
+        intersectObserver.current.observe(post);
+      }
+    },
+    [postsQuery.isLoading, postsQuery.hasNextPage]
+  );
 
   if (postsQuery.isLoading) return <h1>Loading...</h1>;
   if (postsQuery.isError)
@@ -24,15 +52,31 @@ function PostList({}: Props) {
   return (
     <main>
       <h1>PostList (Infinite Scroll Demo)</h1>
-      <ul>
+      <ol>
         {postsQuery.data.pages
           .flatMap((pageData) => pageData.posts)
-          .map((post) => (
-            <li key={post.id}>
-              <PostCard post={post} />
-            </li>
-          ))}
-      </ul>
+          .map((post, index) => {
+            // if this is the last post, attach the lastPostRef
+            if (
+              index ===
+              postsQuery.data.pages.flatMap(
+                (pageData) => pageData.posts
+              ).length -
+                1
+            )
+              return (
+                <li ref={lastPostRef} key={post.id}>
+                  <PostCard post={post} />
+                </li>
+              );
+            else
+              return (
+                <li key={post.id}>
+                  <PostCard post={post} />
+                </li>
+              );
+          })}
+      </ol>
       {postsQuery.hasNextPage ? (
         <button onClick={() => postsQuery.fetchNextPage()}>
           {postsQuery.isFetchingNextPage ? 'Loading...' : 'Show more'}
